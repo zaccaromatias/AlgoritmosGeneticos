5d6c116c2f55aa79dba3d5297181c987a2292425
Franco Luna <franconicolas2007@hotmail.com>
2020-05-07 13:21:46 -0300

    Arreglo de unas salidas en los prints

--- Ejercicio1/Crossover.py
+++ Ejercicio1/Crossover.py
@@ -14,9 +14,9 @@ class Crossover:
         print(
             "//Projenitor1: " + str(int(self.Projenitor1.Valor, 2)) + " --Binario : " + self.Projenitor1.Valor)
         print(
-            "//Projenitor2: " + str(int(self.Projenitor2.Valor)) + " --Binario : " + self.Projenitor2.Valor)
+            "//Projenitor2: " + str(int(self.Projenitor2.Valor, 2)) + " --Binario : " + self.Projenitor2.Valor)
         print(
-            "//Hijo1: " + str(int(self.Hijo1.Valor)) + " --Binario : " + self.Hijo1.Valor)
+            "//Hijo1: " + str(int(self.Hijo1.Valor, 2)) + " --Binario : " + self.Hijo1.Valor)
         print(
-            "//Hijo2: " + str(int(self.Hijo2.Valor)) + " --Binario : " + self.Hijo2.Valor)
+            "//Hijo2: " + str(int(self.Hijo2.Valor, 2)) + " --Binario : " + self.Hijo2.Valor)
         print("//Unidades: " + str(self.Unidades))
