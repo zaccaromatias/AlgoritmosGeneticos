1cefef7f18b6a630eb608be029a075aba3d7e204
moonfrank <franconicolas2007@hotmail.com>
2020-09-16 15:24:09 -0300

    Lectura de datos de excel para mapa implementada

--- Ejercicio3/MapHelper.py
+++ Ejercicio3/MapHelper.py
@@ -11,11 +11,13 @@ from Ejercicio3.DistanciaHelper import DistanciaHelper
 class MapHelper:
     # El orden de las coordenadas es igual al de la lista de capitales
     # Después lo paso al Excel
+    """
     PosicionCapMapa = [(354, 348), (230, 266), (349, 169), (357, 131), (365, 357),
                        (172, 216), (128, 305), (139, 455), (312, 276), (411, 166),
                        (210, 564), (341, 163), (117, 781), (194, 191), (203, 151),
                        (203, 81), (203, 98), (133, 271), (178, 316), (303, 270),
                        (228, 396), (224, 175), (139, 855), (250, 504)]
+    """
 
     @staticmethod
     def DibujarMapa(recorrido: []):
@@ -45,12 +47,14 @@ class MapHelper:
 
         for capital in range(len(recorrido) - 1):
             pg.draw.circle(mapa, ROJO, MapHelper.PosicionCapMapa[capital], 4)
-
+        print(df.Long[3])
+        print()
         for rec in range(len(recorrido)-1):
-            pg.draw.line(mapa, VERDE, MapHelper.PosicionCapMapa[recorrido[rec].Indice],
-                         MapHelper.PosicionCapMapa[recorrido[rec + 1].Indice], 3)
-        pg.draw.line(mapa, VERDE, MapHelper.PosicionCapMapa[recorrido[len(recorrido)-1].Indice],
-                     MapHelper.PosicionCapMapa[recorrido[0].Indice], 3)
+            print(recorrido[rec + 1].Indice)
+            pg.draw.line(mapa, VERDE, (df.Lat[recorrido[rec].Indice], df.Long[recorrido[rec].Indice]),
+                         (df.Lat[recorrido[rec + 1].Indice], df.Long[recorrido[rec + 1].Indice]), 3)
+        pg.draw.line(mapa, VERDE, (df.Lat[recorrido[len(recorrido)-1].Indice], df.Long[recorrido[len(recorrido)-1].Indice]),
+                     (df.Lat[recorrido[0].Indice], df.Long[recorrido[0].Indice]), 3)
         pg.display.update()
 
         EXIT = False
